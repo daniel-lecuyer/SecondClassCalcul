0b12f8ef18a659be5c7a27b7710d2b22f021b2ed
Daniel <dan.irestful@gmail.com>
2013-12-04 10:03:04 -0500

    created project

---
+++ calcul.php
@@ -0,0 +1,10 @@
+<?php
+/*$quantity=32;
+$quantity= 32 * 12;
+$quantity= $quantity + 12;
+$quantity= $quantity - 14;*/
+
+$quantity = 32;
+$quantity = (($quantity * 12) + 12) - 14;
+echo PHP_EOL."The result is: ".$quantity.PHP_EOL;
+
